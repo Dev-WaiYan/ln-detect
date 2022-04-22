66986e6de55aedc7af2b84844dacf718b55f6046
Dev-WaiYan <htetwaiwilsonsprite@gmail.com>
2022-04-23 00:46:43 +0630

    Project setup

---
+++ database/migrations/2022_04_22_175830_create_record_histories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('record_histories', function (Blueprint $table) {
+            $table->id();
+            $table->integer("character_count");
+            $table->integer("word_count");
+            $table->integer("input_string");
+            $table->json("detected_languages");
+            $table->timestamps();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('record_histories');
+    }
+};
