ae38c69e04c1d3c0dbbbadb57fe493208adefcbd
Dev-WaiYan <htetwaiwilsonsprite@gmail.com>
2022-04-23 11:04:55 +0630

    Bug fix: Errorhandler

--- src/error/useErrorHandler.js
+++ src/error/useErrorHandler.js
@@ -1,6 +1,10 @@
 function useErrorHandler() {
   const handleError = (obj) => {
     console.log("error handler : ", obj);
+    if (obj.response.status === 403) {
+      localStorage.clear();
+      window.location.href = "/";
+    }
   };
   return { handleError: handleError };
 }
